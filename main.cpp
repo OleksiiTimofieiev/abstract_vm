1270b95815a92696b4531d17aac6810dd2c9c36c
contrbas42 <contrbas@gmail.com>
2018-07-23 00:55:52 +0300

    s

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@ int		main ( int argc, char ** argv )
 	// TODO: start the project;
 	// TODO: skip the not valid line with continue, use linked list stl;
 	// TODO: how to show all exception cases in the end of the execution;
+	
 	return (0);
 }
