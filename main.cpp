0ca2a5537483e7ca4e46fedf34a09e81a5574e2a
contrbas42 <contrbas@gmail.com>
2018-07-23 00:55:08 +0300

    some new ideas

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 int		main ( int argc, char ** argv )
 {
 	// TODO: start the project;
-	// TODO: skip the not valid line with continue;
+	// TODO: skip the not valid line with continue, use linked list stl;
 	// TODO: how to show all exception cases in the end of the execution;
 	return (0);
 }
