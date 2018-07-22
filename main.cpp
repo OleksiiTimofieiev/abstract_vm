ae0cd9e90acbd5c95fc8919c26678c3961c3714a
contrbas42 <contrbas@gmail.com>
2018-07-23 00:49:45 +0300

    blablabla

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 int		main ( int argc, char ** argv )
 {
+	
 	// TODO: start the project;
 	// TODO: skip the not valid line with continue;
 	// TODO: how to show all exception cases in the end of the execution;
+	
 	return (0);
 }
