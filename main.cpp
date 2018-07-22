7795a3f8d73b3aeee09900b7689a4ca7949decf1
contrbas42 <contrbas@gmail.com>
2018-07-23 00:44:48 +0300

    c

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 int		main ( int argc, char ** argv )
 {
 	// TODO: start the project;
-	// TODO: check out how to set shortcut to the todolist check;
 	// TODO: skip the not valid line with continue;
-	// TODO: how to show all exception cases in the end of the execution;;
+	// TODO: how to show all exception cases in the end of the execution;
 
 	return (0);
 }
