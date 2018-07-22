dc1f257f8a03f7802be654f4a409585bba83b001
contrbas42 <contrbas@gmail.com>
2018-07-23 00:35:18 +0300

    c

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 int		main ( int argc, char ** argv )
 {
 	// TODO: start the project;
-	// TODO: check out how to set shortcut to the todolist check and change the color of the comment to yellow;
+	// TODO: check out how to set shortcut to the todolist check;
 	// TODO: skip the not valid line with continue;
 	// TODO: how to show all exception cases in the end of the execution;;
 
