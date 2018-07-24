c8044630eba74277735d9d250a6abed9670eec06
contrbas@gmail.com <K4rmU78B>
2018-07-24 13:57:40 +0300

    ALL

--- Lexer.cpp
+++ Lexer.cpp
@@ -10,10 +10,8 @@ void	Lexer::show_contents( Parser & parser )
 	std::cout << std::endl;
 
 	if ( !parser._filtered_input.empty() )
-	{
 		for ( auto i = parser._filtered_input.cbegin(); i != parser._filtered_input.cend(); ++i )
 			std::cout << *i << std::endl;
-	}
-	
+
 	std::cout << std::endl;
 }
