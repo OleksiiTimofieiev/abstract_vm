45d907cf6302481668bb69e658f16890cd2387ce
contrbas@gmail.com <K4rmU78B>
2018-07-24 12:00:01 +0300

    ALL

--- CPU.cpp
+++ CPU.cpp
@@ -3,6 +3,8 @@
 CPU::CPU () {}
 CPU::~CPU() {}
 
+//TODO: add copliens form;
+
 void	CPU::execute_inner_logic( const int quantity_of_arguments, char *argument )
 {
 	_parser.read(quantity_of_arguments, argument);
