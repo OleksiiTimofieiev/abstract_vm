a82fcacbc04b26a0b91c847798f30acbcda0f0e5
contrbas@gmail.com <K4rmU78B>
2018-07-24 16:38:02 +0300

    ALL

--- test_exception.cpp
+++ test_exception.cpp
@@ -1,28 +1,46 @@
 #include <iostream>
-#include <exception>
-using namespace std;
+#include <sstream>
+#include <stdexcept>
+#include <string>
 
-struct MyException : public exception
+class my_exception : public std::runtime_error
 {
+	std::string msg;
+
+  	public:
+	my_exception(const std::string &arg, const char *file, int line) : std::runtime_error(arg)
+	{
+		std::ostringstream o;
+		o << file << ":" << line << ": " << arg;
+		msg = o.str();
+	}
+	~my_exception() throw() {}
 	const char *what() const throw()
 	{
-		std::string adf = "asd";
-		return adf.c_str();
+		return msg.c_str();
 	}
 };
+#define throw_line(arg) throw my_exception(arg, __FILE__, __LINE__);
+
+void f()
+{
+	throw_line("some error!");
+}
 
 int main()
 {
-	try
+	int i = 0;
+
+	while ( i < 5)
 	{
-		throw MyException();
-	}
-	catch (MyException &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	catch (std::exception &e)
-	{
-		//Other errors
+		try
+		{
+			f();
+		}
+		catch (const std::runtime_error &ex)
+		{
+			std::cout << ex.what() << std::endl;
+		}
+		i++;
 	}
 }
