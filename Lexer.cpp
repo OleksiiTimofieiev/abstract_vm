3a0717cc8d18ad0c89b70ce1c7240678e3acbd24
contrbas@gmail.com <K4rmU78B>
2018-07-26 12:56:26 +0300

    ALL

--- Lexer.cpp
+++ Lexer.cpp
@@ -12,11 +12,14 @@ bool	Lexer::regex_checks( std::string str )
 	bool flag = false;
 
 	const std::regex regular_expression_for_commands_without_arguments("^(pop|dump|add|sub|mul|div|mod|print|exit|;;)$");
-	const std::regex regular_expression_for_commands_with_arguments("^(push|assert) (int8|int16|int32|float|double)(\\()([-]?[0-9]+)(\\))\\s*$");
+	const std::regex regular_expression_for_commands_with_arguments_int("^(push|assert) (int8|int16|int32)(\\()([-]?[0-9]+)(\\))\\s*$");
+	const std::regex regular_expression_for_commands_with_arguments_float_double("^(push|assert) (float|double)(\\()([-]?[0-9]+)(\\))\\s*$");
 
 	if (std::regex_match(str, regular_expression_for_commands_without_arguments))
 		flag = true;
-	else if ((std::regex_match(str, regular_expression_for_commands_with_arguments)))
+	else if ((std::regex_match(str, regular_expression_for_commands_with_arguments_int)))
+		flag = true;
+	else if ((std::regex_match(str, regular_expression_for_commands_with_arguments_float_double)))
 		flag = true;
 	return (flag);
 }
