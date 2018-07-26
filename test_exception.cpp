687c77caa56e44963dd3906989ce518e65f59277
contrbas@gmail.com <K4rmU78B>
2018-07-26 16:19:53 +0300

    ALL

--- test_exception.cpp
+++ test_exception.cpp
@@ -1,24 +1,42 @@
+#include <exception>
 #include <iostream>
-#include <stdexcept>
 
-class GolfError : public std::runtime_error
+class Exception : public std::exception
 {
   public:
-	GolfError(const char *what) : runtime_error(what) {}
+	explicit Exception(const char *message) : msg_(message)
+	{
+	}
+	explicit Exception(const std::string &message) : msg_(message)
+	{
+	}
+	/** Destructor.
+     * Virtual to allow for subclassing.
+     */
+	virtual ~Exception() throw() {}
+
+	/** Returns a pointer to the (constant) error description.
+     *  @return A pointer to a const char*. The underlying memory
+     *          is in posession of the Exception object. Callers must
+     *          not attempt to free the memory.
+     */
+	virtual const char *what() const throw()
+	{
+		return msg_.c_str();
+	}
+
+  protected:
+	std::string msg_;
 };
 
 int main()
 {
-	int x = 8;
-
 	try
 	{
-		x = 9;
-		if (x == 9)
-			throw GolfError("asdfadf");
+		throw Exception("Something went wrong...\n");
 	}
-	catch (GolfError & e)
+	catch (Exception & ex)
 	{
-		std::cout << e.what();
+		std::cout << ex.what() << std::endl;
 	}
 }
