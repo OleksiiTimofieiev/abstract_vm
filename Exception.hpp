b8a5546e59da6e9b991fafcbf48703d051370cf0
contrbas@gmail.com <K4rmU78B>
2018-07-26 16:59:28 +0300

    ALL

--- Exception.hpp
+++ Exception.hpp
@@ -1,33 +1,38 @@
-#ifndef EXCEPTION_CPP
-#define	EXCEPTION_CPP
+#ifndef EXCEPTION_HPP
+#define EXCEPTION_HPP
 
-#include <exception>
 #include <iostream>
+#include <exception>
+#include <string>
 
-class Exception : public std::exception
+class my_exception : public std::runtime_error
 {
-  private:
+	std::string _msg;
+	std::string _result;
 	int _line;
 
   public:
-	const char *what()
+	my_exception(const std::string &arg, int line) : std::runtime_error(arg)
 	{
-		return ("");
+		_msg = arg;
+		_line = line;
+
+		_msg = _msg + std::to_string(_line);
+	}
+
+	~my_exception() throw() {}
+
+	const char *what() const throw()
+	{
+		return _msg.c_str();
 	}
 };
 
+#define throw_line(arg, line) throw my_exception(arg, line);
 
-
-
-
-
-
-
-
-
-
-
-
-
+void f(int line)
+{
+	throw_line("\033[1;31mlexer error type 1 -> not valid command !\033[0m", line);
+}
 
 #endif
