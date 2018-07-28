6c7f875ff7e0ad05251339963a3d27418a4ead89
contrbas@gmail.com <K4rmU78B>
2018-07-28 14:08:50 +0300

    ALL

--- vector_of_functions_tests.cpp
+++ vector_of_functions_tests.cpp
@@ -3,7 +3,7 @@
 #include <functional>
 
 // for the syntax simplification;
-typedef std::function<void(int, int)> list;
+typedef std::function<void(int, int)> function_object;
 
 // enum creation;
 enum function {
@@ -19,14 +19,14 @@ void	create_int( int var, int value )
 
 void	create_float( int var, int value )
 {
-	std::cout << var * value << std::endl;
+	std::cout << var * (value * 10) << std::endl;
 }
 
 int		main ( void )
 {
 	std::vector<int> vc;
 
-	std::vector<list> vFunc;
+	std::vector<function_object> vFunc;
 
 	vFunc.push_back(create_int);
 	vFunc.push_back(create_float);
