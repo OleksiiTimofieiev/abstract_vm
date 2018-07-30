75a318a1a9f56714a96eece463dfd6150a81ea5b
contrbas@gmail.com <K4rmU78B>
2018-07-30 13:45:01 +0300

    ALL

--- array_of_arrays.cpp
+++ array_of_arrays.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <array>
+#include <vector>
 
 
 
@@ -24,28 +25,36 @@
 
 int		main( void )
 {
+	int				size = 4;
+
 	std::string		command = "push";
 	std::string 	type = "int8";
 	std::string 	value = "42";
 
-	std::array<std::array<std::string,3>, 3> arr;
+	std::vector<std::vector<std::string> > arr(size);
 
-	arr[0][0] = command;
-	arr[0][1] = type;
-	arr[0][2] = value;
+	arr[0].push_back(command);
+	arr[0].push_back(type);
+	arr[0].push_back(value);
 
-	arr[1][0] = command;
-	arr[1][1] = type;
-	arr[1][2] = value;
+	// arr[0][0] = command;
+	// arr[0][1] = type;
+	// arr[0][2] = value;
 
-	arr[2][0] = command;
-	arr[2][1] = type;
-	arr[2][2] = value;
+	// arr[1][0] = command;
+	// arr[1][1] = type;
+	// arr[1][2] = value;
+
+	// arr[2][0] = command;
+	// arr[2][1] = type;
+	// arr[2][2] = value;
 
 	for (int i = 0; i < 3; i++)
 	{
 		std::cout << arr[0][i] << std::endl;
 	}
+
+	system("leaks -q a.out");
 
 	return (0);
 }
