6d4ab3e3f221a5d61e263a723214d1e0e81d6fa6
contrbas@gmail.com <K4rmU78B>
2018-07-31 11:44:22 +0300

    ALL

--- IOperand.hpp
+++ IOperand.hpp
@@ -1,26 +1,25 @@
 #include <iostream>
 
-
+enum eOperandType
+{
+	Int8 = 0,
+	Int16,
+	Int32,
+	Float,
+	Double,
+};
 
 class IOperand
 { 
 private:
-  	enum eOperandType
-	{
-		Int8 = 0,
-		Int16,
-		Int32,
-		Float,
-		Double,
-	};
 public:
-  virtual int getPrecision(void) const = 0;
-  virtual eOperandType getType(void) const = 0;
-  virtual IOperand const *operator+(IOperand const &rhs) const = 0;
-  virtual IOperand const *operator-(IOperand const &rhs) const = 0;
-  virtual IOperand const *operator*(IOperand const &rhs) const = 0;
-  virtual IOperand const *operator/(IOperand const &rhs) const = 0;
-  virtual IOperand const *operator%(IOperand const &rhs) const = 0;
+  virtual int				getPrecision(void) const = 0;
+  virtual eOperandType 		getType(void) const = 0;
+  virtual IOperand const *	operator+(IOperand const &rhs) const = 0;
+  virtual IOperand const *	operator-(IOperand const &rhs) const = 0;
+  virtual IOperand const *	operator*(IOperand const &rhs) const = 0;
+  virtual IOperand const *	operator/(IOperand const &rhs) const = 0;
+  virtual IOperand const *	operator%(IOperand const &rhs) const = 0;
 
   virtual std::string const &toString(void) const = 0;
 
