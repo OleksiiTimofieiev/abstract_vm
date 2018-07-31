2b5346fb79341aaf3aee4a0787eb6d0b151c31ce
contrbas@gmail.com <K4rmU78B>
2018-07-31 11:53:24 +0300

    ALL

--- IOperand.hpp
+++ IOperand.hpp
@@ -1,27 +1,25 @@
-#include <iostream>
+#ifndef IOPERAND_HPP
+#define IOPERAND_HPP
 
-enum eOperandType
-{
-	Int8 = 0,
-	Int16,
-	Int32,
-	Float,
-	Double,
-};
+#include <string>
+
+enum eOperandType {Int8, Int16, Int32, Float, Double};
 
 class IOperand
 { 
 private:
 public:
-  virtual int				getPrecision(void) const = 0;
-  virtual eOperandType 		getType(void) const = 0;
+  virtual int				getPrecision(void) const = 0;				// add it to the constructor with private variable;
+  virtual eOperandType 		getType(void) const = 0;					// different	func;
   virtual IOperand const *	operator+(IOperand const &rhs) const = 0;
   virtual IOperand const *	operator-(IOperand const &rhs) const = 0;
   virtual IOperand const *	operator*(IOperand const &rhs) const = 0;
   virtual IOperand const *	operator/(IOperand const &rhs) const = 0;
   virtual IOperand const *	operator%(IOperand const &rhs) const = 0;
 
-  virtual std::string const &toString(void) const = 0;
+  virtual std::string const &toString(void) const = 0;					// int to string 
 
   virtual ~IOperand(void){};
 };
+
+#endif
