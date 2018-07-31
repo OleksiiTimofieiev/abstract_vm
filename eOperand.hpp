3432a293d0328c548e6f0c961007d88d3e348555
contrbas@gmail.com <K4rmU78B>
2018-07-31 15:05:53 +0300

    ALL

--- eOperand.hpp
+++ eOperand.hpp
@@ -5,71 +5,73 @@
 
 // private / protected variables;
 //TODO:copl form
+//TODO:exceptions
 
 template <typename T>
 
 class eOperand : public IOperand
 {
-public:
-	int 			getPrecision( void ) const
-	{
-		std::string type = typeid(T).name();
+	public:
+		int 			getPrecision( void ) const
+		{
+			std::string type = typeid(T).name();
 
-		if (type == "c")
-			return (Int8);
-		else if (type == "s")
-			return (Int16);
-		else if (type == "i")
-			return (Int32);
-		else if (type == "f")
-			return (Float);
-		else if (type == "d")
-			return (Double);
+			if (type == "c")
+				return (Int8);
+			else if (type == "s")
+				return (Int16);
+			else if (type == "i")
+				return (Int32);
+			else if (type == "f")
+				return (Float);
+			else if (type == "d")
+				return (Double);
 
-		return (default_value);
-	}
-	eOperandType 	getType( void ) const
-	{
-		std::string type = typeid(T).name();
+			return (default_value);
+		}
+		eOperandType 	getType( void ) const
+		{
+			std::string type = typeid(T).name();
 
-		if (type == "c")
-			return (Int8);
-		else if (type == "s")
-			return (Int16);
-		else if (type == "i")
-			return (Int32);
-		else if (type == "f")
-			return (Float);
-		else if (type == "d")
-			return (Double);
+			if (type == "c")
+				return (Int8);
+			else if (type == "s")
+				return (Int16);
+			else if (type == "i")
+				return (Int32);
+			else if (type == "f")
+				return (Float);
+			else if (type == "d")
+				return (Double);
 
-		return (default_value);
-	}
-	// IOperand const *operator+(IOperand const &rhs) const;
-	// IOperand const *operator-(IOperand const &rhs) const;
-	// IOperand const *operator*(IOperand const &rhs) const;
-	// IOperand const *operator/(IOperand const &rhs) const;
-	// IOperand const *operator%(IOperand const &rhs) const;
+			return (default_value);
+		}
 
-	std::string const &toString(void) const
-	{
-		return (_value);
-	}
-	eOperand( void ) {};
-	~eOperand( void ) {};
-	eOperand(const std::string & str) : _value(str)
-	{
-		std::string type;
+		// IOperand const *operator+(IOperand const &rhs) const;
+		// IOperand const *operator-(IOperand const &rhs) const;
+		// IOperand const *operator*(IOperand const &rhs) const;
+		// IOperand const *operator/(IOperand const &rhs) const;
+		// IOperand const *operator%(IOperand const &rhs) const;
 
-		type = typeid(T).name();
+		std::string const &toString(void) const
+		{
+			return (_value);
+		}
+		eOperand(const std::string & str) : _value(str)
+		{
+			std::string type;
 
-		if (type == "i")
-			_value_after_conversion = stoi(str);
-	}
-	
-private:
-	std::string		_value;
-	T				_value_after_conversion;
+			type = typeid(T).name();
+
+			if (type == "i")
+				_value_after_conversion = stoi(str);
+		}
+		eOperand( void ) {};
+		~eOperand( void ) {};
+		
+	private:
+		std::string		_value;
+		T				_value_after_conversion;
 };
 
 #endif
