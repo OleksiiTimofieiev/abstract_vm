b932058d7feee86a1bbb8fd07e1252911fd5c98d
contrbas@gmail.com <K4rmU78B>
2018-07-31 11:36:21 +0300

    ALL

--- IOperand.hpp
+++ IOperand.hpp
@@ -1,26 +1,29 @@
 #include <iostream>
 
-enum eOperandType
-{
-	Int8 = 0,
-	Int16,
-	Int32,
-	Float,
-	Double,
-};
+
 
 class IOperand
 { 
-	public:
-		virtual int					getPrecision( void ) const = 0;
-		virtual eOperandType		getType( void ) const = 0;
-		virtual IOperand const * 	operator+( IOperand const & rhs ) const = 0;
-		virtual IOperand const * 	operator-( IOperand const & rhs ) const = 0;
-		virtual IOperand const * 	operator*( IOperand const & rhs ) const = 0;
-		virtual IOperand const * 	operator/( IOperand const & rhs ) const = 0;
-		virtual IOperand const * 	operator%( IOperand const & rhs ) const = 0;
+private:
+  enum eOperandType
+  {
+	  Int8 = 0,
+	  Int16,
+	  Int32,
+	  Float,
+	  Double,
+  };
 
-		virtual std::string const & toString( void ) const = 0;
+public:
+  virtual int getPrecision(void) const = 0;
+  virtual eOperandType getType(void) const = 0;
+  virtual IOperand const *operator+(IOperand const &rhs) const = 0;
+  virtual IOperand const *operator-(IOperand const &rhs) const = 0;
+  virtual IOperand const *operator*(IOperand const &rhs) const = 0;
+  virtual IOperand const *operator/(IOperand const &rhs) const = 0;
+  virtual IOperand const *operator%(IOperand const &rhs) const = 0;
 
-		virtual ~IOperand( void ) {};
+  virtual std::string const &toString(void) const = 0;
+
+  virtual ~IOperand(void){};
 };
