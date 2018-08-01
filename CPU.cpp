94924a272865d3640c642d1d40e3618f38644aeb
contrbas@gmail.com <K4rmU78B>
2018-08-01 18:19:26 +0300

    ALL

--- CPU.cpp
+++ CPU.cpp
@@ -9,28 +9,9 @@ CPU::~CPU() {}
 
 void	CPU::execute_inner_logic(const int quantity_of_arguments, char *argument)
 {
-	_parser.read			( quantity_of_arguments, argument );
-	_lexer.lexical_analysis	( _parser, _command_list );
-
-
-	// output(_command_list[0][0]);
-	// output(_command_list[0][1]);
-	// output(_command_list[0][2]);
-	// output("");
-
-	// output(_command_list[1][0]);
-	// output(_command_list[1][1]);
-	// output(_command_list[1][2]);
-	// output("");
-
-	// output(_command_list[2][0]);
-	// output(_command_list[2][1]);
-	// output(_command_list[2][2]);
-	// output("");
-
-	//FIXME:: delete in the end;
-
-	_commands_execution_routine();
+	_parser.read				( quantity_of_arguments, argument );
+	_lexer.lexical_analysis		( _parser, _command_list );
+	_commands_execution_routine	();
 
 	_parser.detect_leaks(); // detect leaks;
 }
