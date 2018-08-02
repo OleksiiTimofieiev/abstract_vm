23fbabb68f40a6b9d7dfdc342cb19aa111ace908
contrbas@gmail.com <K4rmU78B>
2018-08-02 15:53:21 +0300

    ALL

--- eOperand.cpp
+++ eOperand.cpp
@@ -1,29 +1,22 @@
 #include "eOperand.hpp"
 
-//TODO:// 	// int0, float7 ,double16; str::strstream -> set precision st to string;
+//TODO:// 	// int0, float7 ,double14; str::strstream -> set precision st to string;
 //TODO:: not a value;
+	//TODO::check input value;
 
 template <class T> eOperand<T>::eOperand(const std::string &str) : _value(str)
 {
-	std::string type;
-
-	type = typeid(T).name();
-
-	//TODO::check input value;
-	// if (typeid(T) == typeid(int8_t))//
-	if (type == "a")
+	if (typeid(T) == typeid(int8_t))
 		_value_after_conversion = stoi(str);
-	else if (type == "s")
+	else if (typeid(T) == typeid(int16_t))
 		_value_after_conversion = stoi(str);
-	else if (type == "i")
+	else if (typeid(T) == typeid(int32_t))
 		_value_after_conversion = stoi(str);
-	else if (type == "f")
+	else if (typeid(T) == typeid(float))
 		_value_after_conversion = stof(str);
-	else if (type == "d")
+	else if (typeid(T) == typeid(double))
 		_value_after_conversion = stod(str);
-
-
-	}
+}
 template <class T> eOperand<T>::eOperand(void) {}
 template <class T> eOperand<T>::~eOperand(void) {}
 template <class T> IOperand const& eOperand<T>::operator = (eOperand const &rhs)
@@ -42,36 +35,30 @@ template <class T> eOperand<T>::eOperand(eOperand const &src)
 
 template <class T> int eOperand<T>::getPrecision(void) const
 {
-	std::string type = typeid(T).name();
-
-	if (type == "a")
-		return (Int8);
-	else if (type == "s")
-		return (Int16);
-	else if (type == "i")
-		return (Int32);
-	else if (type == "f")
-		return (Float);
-	else if (type == "d")
-		return (Double);
-
-	return (default_value);
+	if (typeid(T) == typeid(int8_t))
+		return(0);
+	else if (typeid(T) == typeid(int16_t))
+		return(0);
+	else if (typeid(T) == typeid(int32_t))
+		return(0);
+	else if (typeid(T) == typeid(float))
+		return(7);
+	else if (typeid(T) == typeid(double))
+		return(14);
+	return (0);
 }
 template <class T> eOperandType eOperand<T>::getType(void) const
 {
-	std::string type = typeid(T).name();
-
-	if (type == "a")
-		return (Int8);
-	else if (type == "s")
-		return (Int16);
-	else if (type == "i")
-		return (Int32);
-	else if (type == "f")
-		return (Float);
-	else if (type == "d")
-		return (Double);
-
+	if (typeid(T) == typeid(int8_t))
+		return(Int8);
+	else if (typeid(T) == typeid(int16_t))
+		return(Int16);
+	else if (typeid(T) == typeid(int32_t))
+		return(Int32);
+	else if (typeid(T) == typeid(float))
+		return(Float);
+	else if (typeid(T) == typeid(double))
+		return(Double);
 	return (default_value);
 }
 
@@ -102,7 +89,6 @@ template <class T>std::string const &eOperand<T>::toString(void) const
 {
 	return (_value);
 }
-
 
 template class eOperand<int8_t>;
 template class eOperand<int16_t>;
