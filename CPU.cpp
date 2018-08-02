120b15ffc2e7c59224992ef410711bdbf9810825
contrbas@gmail.com <K4rmU78B>
2018-08-02 15:30:46 +0300

    ALL

--- CPU.cpp
+++ CPU.cpp
@@ -2,12 +2,11 @@
 #include "Factory.hpp"
 
 CPU::CPU () {}
+
 CPU::~CPU() 
 {
 	for ( auto i = _stack.begin(); i != _stack.end(); ++i )
-	{
 		delete *i;
-	}
 }
 
 CPU &CPU::operator=(CPU const &rhs)
@@ -39,60 +38,21 @@ void	CPU::_commands_execution_routine()
 	std::vector<std::vector<std::string> >::iterator row_command_list;
 	std::vector<std::string>::iterator col_command_list;
 
-	bool	exit_command_is_pesent_in_command_list = false;
+	bool exit_command_is_pesent_in_command_list = false;
 
-	for 	( row_command_list = _command_list.begin(); row_command_list != _command_list.end(); row_command_list++ )
+	for ( row_command_list = _command_list.begin(); row_command_list != _command_list.end(); row_command_list++ )
 	{
-			col_command_list = row_command_list->begin();
+		col_command_list = row_command_list->begin();
 
-			if (*col_command_list == "exit")
-			{
-				exit_command_is_pesent_in_command_list = true;
-				_exit( );
-			}
+		if (*col_command_list == "exit")
+		{
+			exit_command_is_pesent_in_command_list = true;
+			_exit( );
+		}
 	}
 
-	if ( 	!exit_command_is_pesent_in_command_list )
-			OUTPUT_RED( "No exit command available." );
-		
-
-
-		
-
-	// function with default params for the array of the functions;
-	// enum for the functions
-
-	// eOperand<int8_t> a("65");
-	// eOperand<int16_t> b("2");
-	// eOperand<int32_t> c("3");
-	// eOperand<float> d("4");
-	// eOperand<double> e("5");
-
-	// a = c;
-
-	// OUTPUT(a.toString());
-	// OUTPUT("");
-
-	_stack.push_back(_factory.createOperand(Int8, "0"));
-	_stack.push_back(_factory.createOperand(Int8, "1"));
-	_stack.push_back(_factory.createOperand(Int32, "65"));
-	// _stack.push_back(c + b);
-
-	// OUTPUT(_stack[0]->toString());
-	// OUTPUT(_stack[1]->toString());
-	// OUTPUT(_stack[2]->toString());
-	// OUTPUT(_stack[3]->toString()); 
-
-	// OUTPUT("");
-
-	// IOperand  const * z1 = _stack.back();
-	// std::string sample = z1->toString();
-	// _stack.pop_back();
-	// _stack.pop_back();
-
-	// OUTPUT(_stack.back()->toString());
-	// OUTPUT(_stack.back()->getType());
-
+	if ( !exit_command_is_pesent_in_command_list )
+		OUTPUT_RED( "No exit command available." );
 }
 bool	CPU::_print( void )
 {
