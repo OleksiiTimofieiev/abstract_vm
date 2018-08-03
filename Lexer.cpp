40134f75d07eeb763e747b7d128215badc0fe577
contrbas@gmail.com <K4rmU78B>
2018-08-03 22:18:54 +0300

    ALL

--- Lexer.cpp
+++ Lexer.cpp
@@ -14,8 +14,8 @@ bool Lexer::regex_checks(std::string str, std::vector<std::vector<std::string> >
 
 	std::smatch		result;
 	const 			std::regex	commands_without_arguments("^(pop|dump|add|sub|mul|div|mod|print|exit|;;)$");
-	const 			std::regex	commands_with_arguments_int("^(push|assert) (int8|int16|int32)\\(([-]?[0-9]+)\\)$");
-	const 			std::regex	commands_with_arguments_float_double("^(push|assert) (float|double)\\(([-]?[0-9]+\\.[0-9]+)\\)$");
+	const 			std::regex	commands_with_arguments_int("^(push|assert) (int8|int16|int32)\\(([\\-]?[0-9]+)\\)$");
+	const 			std::regex	commands_with_arguments_float_double("^(push|assert) (float|double)\\(([\\-]?[0-9]+\\.[0-9]+)\\)$");
 	const			std::regex	empty_line("^\\s*$");
 
 	if (std::regex_match( str, result, commands_without_arguments) || std::regex_match( str, result, empty_line))
