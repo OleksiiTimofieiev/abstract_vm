09cb0abd4eea2a51f5c127c898d9fa87fee41ab2
contrbas42 <contrbas@gmail.com>
2018-08-06 02:54:32 +0300

    c

--- source/main.cpp
+++ source/main.cpp
@@ -3,20 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: timofieiev <timofieiev@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/05 21:57:29 by otimofie          #+#    #+#             */
-/*   Updated: 2018/08/05 21:57:30 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/08/06 02:54:26 by timofieiev       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/CPU.hpp"
 
-int		main ( int argc, char ** argv )
-{
-	CPU _cpu;
-
-	_cpu._execute_inner_logic( argc, argv[1] );
-
-	return (0);
-}
+int		main ( int argc, char ** argv ) { CPU()._execute_inner_logic( argc, argv[1] ); return (0); }
