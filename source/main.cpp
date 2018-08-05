cd4f05fb2be8d6a4d7ffd9acd06691bd9ea03ee8
contrbas42 <contrbas@gmail.com>
2018-08-06 02:44:03 +0300

    c

--- source/main.cpp
+++ source/main.cpp
@@ -12,18 +12,11 @@
 
 #include "../includes/CPU.hpp"
 
-int		main2 ( int argc, char ** argv )
+int		main ( int argc, char ** argv )
 {
 	CPU _cpu;
 
 	_cpu._execute_inner_logic( argc, argv[1] );
 
-	return (0);
-}
-
-int		main( int argc, char ** argv )
-{
-	main2(argc, argv);
-	system("leaks -q avm");
 	return (0);
 }
